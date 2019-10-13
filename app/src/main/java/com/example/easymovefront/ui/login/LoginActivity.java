bcd652e758886626c2874ba87655d1113189a2ae
Adem <adem998@gmail.com>
2019-10-13 14:57:28 +0200

    1er commit ui

--- app/src/main/java/com/example/easymovefront/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/easymovefront/ui/login/LoginActivity.java
@@ -33,8 +33,8 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
+        final EditText usernameEditText = findViewById(R.id.email);
+        final EditText passwordEditText = findViewById(R.id.txtPassword);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
