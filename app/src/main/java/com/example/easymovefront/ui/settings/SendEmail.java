76d5b6ae4f38e6fe9e47359fff2d153745ddba6b
Adem <adem998@gmail.com>
2020-01-02 13:39:40 +0100

    feedback jar not working

--- app/src/main/java/com/example/easymovefront/ui/settings/SendEmail.java
+++ app/src/main/java/com/example/easymovefront/ui/settings/SendEmail.java
@@ -5,7 +5,6 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -17,7 +16,7 @@ public class SendEmail {
     static Session getMailSession;
     static MimeMessage generateMailMessage;
 
-    public static void generateAndSendEmail(String body) throws AddressException, MessagingException {
+    public static void generateAndSendEmail(String body) throws MessagingException {
 
         // Paso1 (no hace falta modificar puertos)
         System.out.println("\n 1 ===> Guardando propiedades del servidor");
